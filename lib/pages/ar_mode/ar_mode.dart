c06c868e390f9fe58e8d1e93a80f20f136dff4fd
Muhibush <ukiacrew@gmail.com>
2020-08-23 10:09:51 +0700

    navigate to ARMode

--- lib/pages/ar_mode/ar_mode.dart
+++ lib/pages/ar_mode/ar_mode.dart
@@ -14,7 +14,7 @@ class ARMode extends StatefulWidget {
 class _ARModeState extends State<ARMode> {
   ArCoreController arCoreController;
 
-  void _onArCoreViewCreated(ArCoreController _arCoreController){
+  void _onArCoreViewCreated(ArCoreController _arCoreController) {
     arCoreController = _arCoreController;
     _addSphere(arCoreController);
     _addCube(arCoreController);
@@ -22,46 +22,50 @@ class _ARModeState extends State<ARMode> {
   }
 
   void _addSphere(ArCoreController _arCoreController) {
-    final material = ArCoreMaterial(color: Colors.deepPurple, );
-    final sphere = ArCoreSphere(materials: [material], radius: 0.2,);
+    final material = ArCoreMaterial(
+      color: Colors.deepPurple,
+    );
+    final sphere = ArCoreSphere(
+      materials: [material],
+      radius: 0.2,
+    );
     final node = ArCoreNode(
       shape: sphere,
-      position: vector.Vector3(
-          0, -0.3, -1
-      ),
+      position: vector.Vector3(0, -0.3, -1),
     );
     _arCoreController.addArCoreNode(node);
   }
 
   void _addCylinder(ArCoreController _arCoreController) {
     final material = ArCoreMaterial(color: Colors.green, reflectance: 1);
-    final cylinder =
-    ArCoreCylinder(materials: [material], radius: 0.3,
-      height: 0.1,);
+    final cylinder = ArCoreCylinder(
+      materials: [material],
+      radius: 0.3,
+      height: 0.1,
+    );
     final node = ArCoreNode(
       shape: cylinder,
-      position: vector.Vector3(
-          -0.3, -1, -1.0
-      ),
+      position: vector.Vector3(-0.3, -1, -1.0),
     );
     _arCoreController.addArCoreNode(node);
   }
 
   _addCube(ArCoreController _arCoreController) {
     final material = ArCoreMaterial(color: Colors.pink, metallic: 1);
-    final cube = ArCoreCube(materials: [material], size: vector.Vector3(0.5, 0.5, 0.5));
+    final cube =
+        ArCoreCube(materials: [material], size: vector.Vector3(0.5, 0.5, 0.5));
     final node = ArCoreNode(
       shape: cube,
-      position: vector.Vector3(
-          0.5, -3, -3
-      ),
+      position: vector.Vector3(0.5, -3, -3),
     );
     _arCoreController.addArCoreNode(node);
   }
 
   @override
   void dispose() {
-    arCoreController.dispose();
+    try {
+      arCoreController.dispose();
+    } catch (_) {}
     super.dispose();
   }
 
